502fdcde4314da20f69f7c0f6056463a201bb86b
KentFujii <kent.where.the.light.is@gmail.com>
2019-12-09 22:13:13 +0900

    fix

--- app/scheduler/CodeBlockTaskScheduler.scala
+++ app/scheduler/CodeBlockTaskScheduler.scala
@@ -1,15 +1,18 @@
 package tasks
 
 import javax.inject.Inject
+import play.api.inject.SimpleModule
+import play.api.inject._
 
 import akka.actor.ActorSystem
-
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import play.Logger
 
+class TasksModule extends SimpleModule(bind[CodeBlockTask].toSelf.eagerly())
+
 class CodeBlockTask @Inject() (actorSystem: ActorSystem)(implicit executionContext: ExecutionContext) {
-  actorSystem.scheduler.schedule(initialDelay = 10.seconds, interval = 1.minute) {
+  actorSystem.scheduler.schedule(initialDelay = 0.seconds, interval = 10.seconds) {
     Logger.debug("Executing something...");
   }
 }
--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -4,42 +4,41 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.libs.json.Json
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
-class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class HealthControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+  "HealthController GET" should {
+    "Return the healthcheck json response from a new instance of controller" in {
+      val controller = new HealthController(stubControllerComponents())
+      val health = controller.health().apply(FakeRequest(GET, "/health"))
+      val responseJson = Json.parse(contentAsString(health))
 
-  "HomeController GET" should {
-
-    "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      status(health) mustBe OK
+      contentType(health) mustBe Some("application/json")
+      contentAsString(health) must include ("status")
+      (responseJson \ "status").as[Int] mustBe (200)
     }
 
-    "render the index page from the application" in {
-      val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+    "Return the healthcheck json response from the application" in {
+      val controller = inject[HealthController]
+      val health = controller.health().apply(FakeRequest(GET, "/health"))
+      val responseJson = Json.parse(contentAsString(health))
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      status(health) mustBe OK
+      contentType(health) mustBe Some("application/json")
+      contentAsString(health) must include ("status")
+      (responseJson \ "status").as[Int] mustBe (200)
     }
 
-    "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
-      val home = route(app, request).get
+    "Return the healthcheck json response from the router" in {
+      val request = FakeRequest(GET, "/health")
+      val health = route(app, request).get
+      val responseJson = Json.parse(contentAsString(health))
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      status(health) mustBe OK
+      contentType(health) mustBe Some("application/json")
+      contentAsString(health) must include ("status")
+      (responseJson \ "status").as[Int] mustBe (200)
     }
   }
 }
