25737f0df0295301e54772e9c4eced6d9efcd9d2
KentFujii <kent.where.the.light.is@gmail.com>
2019-12-22 01:18:08 +0900

    fix: sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,4 +1,6 @@
 # --- !Ups
 CREATE TABLE `messages` (
-  `text` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8;
