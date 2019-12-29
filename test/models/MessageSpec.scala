dc31923668493625e62d212d1e2a49f52d3823f7
KentFujii <kent.where.the.light.is@gmail.com>
2019-12-30 00:50:09 +0900

    wip: mock_database

--- test/models/MessageSpec.scala
+++ test/models/MessageSpec.scala
@@ -21,24 +21,31 @@ import scala.language.postfixOps
 // class MessageSpec extends PlaySpec with MockitoSugar {
 class MessageSpec extends PlaySpec {
   "Message#list" should {
-    val app = new GuiceApplicationBuilder().build
-    val dbConfigProvider = app.injector.instanceOf[DatabaseConfigProvider]
-    val dbConfig = dbConfigProvider.get[JdbcProfile]
-    val db = dbConfig.db
-    val messages = TableQuery[MessageTable]
-    println(messages.schema.createStatements.mkString)
-    val sampleText = messages.filter(_.text === "sample text!!!")
-    println(sampleText.result.statements.mkString)
-    def exec[T](program: DBIO[T]): T = Await.result(db.run(program), 2 seconds)
-    exec(sampleText.result).foreach { println }
-    val freshMoreTestData = Seq(
-      ("Hello, HAL. Do you read me, HAL?"),
-      ("Affirmative, Dave. I read you."),
-      ("Open the pod bay doors, HAL."),
-      ("I'm sorry, Dave. I'm afraid I can't do that.")
-    )
-    val moreMessages = messages.map(m => (m.text)) ++= freshTestData
-    exec(moreMessages)
-    exec(sampleText.result).foreach { println }
+    "Access real database" in {
+      val app = new GuiceApplicationBuilder().build
+      val dbConfigProvider = app.injector.instanceOf[DatabaseConfigProvider]
+      val dbConfig = dbConfigProvider.get[JdbcProfile]
+      val db = dbConfig.db
+      val messages = TableQuery[MessageTable]
+      println(messages.schema.createStatements.mkString)
+      val sampleText = messages.filter(_.text === "sample text!!!")
+      println(sampleText.result.statements.mkString)
+      def exec[T](program: DBIO[T]): T = Await.result(db.run(program), 2 seconds)
+      exec(sampleText.result).foreach { println }
+      val freshTestData = Seq(
+        ("Hello, HAL. Do you read me, HAL?"),
+        ("Affirmative, Dave. I read you."),
+        ("Open the pod bay doors, HAL."),
+        ("I'm sorry, Dave. I'm afraid I can't do that.")
+      )
+      val moreMessages = messages.map(m => (m.text)) ++= freshTestData
+      exec(moreMessages)
+      exec(messages.result).foreach { println }
+    }
+    "Access mock database" in {
+      // https://stackoverflow.com/questions/34864093/is-it-possible-to-mock-db-connection-in-play-functional-testing-and-how
+      // https://www.playframework.com/documentation/ja/2.4.x/ScalaTestingWithScalaTest
+      println(11111)
+    }
   }
 }
