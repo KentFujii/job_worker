012594a326c4337786b9ea4cebf8acf29fc58c73
KentFujii <kent.where.the.light.is@gmail.com>
2020-01-13 00:48:18 +0900

    wip: mock_db

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ scalaVersion := "2.13.1"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+libraryDependencies += "org.mockito" % "mockito-core" % "3.2.4" % Test
 libraryDependencies += "net.debasishg" %% "redisclient" % "3.20"
 libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.41"
 libraryDependencies += "com.typesafe.play" %% "play-slick" % "5.0.0"
--- test/models/MessageSpec.scala
+++ test/models/MessageSpec.scala
@@ -1,9 +1,12 @@
 package models
 
+import org.scalatest._
 import org.scalatestplus.play._
+import org.scalatestplus.mockito.MockitoSugar
 import play.api.mvc._
 import play.api.test._
 import play.api.test.Helpers._
+import org.mockito.Mockito._
 
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.db.slick.DatabaseConfigProvider
@@ -12,9 +15,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class MessageSpec extends PlaySpec {
+class MessageSpec extends PlaySpec with MockitoSugar {
   "Message#list" should {
-    "Access " in {
+    "Return messages" in {
       // https://www.playframework.com/documentation/ja/2.4.x/ScalaTestingWithScalaTest
       // http://www.scalatest.org/user_guide/testing_with_mock_objects
       // https://github.com/playframework/play-samples/tree/2.8.x/play-scala-slick-example
@@ -22,11 +25,12 @@ class MessageSpec extends PlaySpec {
       // http://unittesting1.blogspot.com/2015/07/mocking-database-with-slick-in.html
       // TODO DatabaseConfigProviderをmockする
       val app = new GuiceApplicationBuilder().build
-      val dbConfigProvider = app.injector.instanceOf[DatabaseConfigProvider]
-      val model = new MessageRepository(dbConfigProvider)
-      Await.result(model.create("test message!!"), 2.seconds)
-      val messages = Await.result(model.list(), 2.seconds)
-      println(messages)
+      val databasebConfigProvider = mock[DatabaseConfigProvider]
+      // val databasebConfigProvider = app.injector.instanceOf[DatabaseConfigProvider]
+      // val model = new MessageRepository(databasebConfigProvider)
+      // Await.result(model.create("test message!!"), 2.seconds)
+      // val messages = Await.result(model.list(), 2.seconds)
+      // println(messages)
     }
   }
 }
