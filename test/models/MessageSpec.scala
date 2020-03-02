0a84ab0d4eeff808a5f44a9fe111a81ef60feac3
KentFujii <kent.where.the.light.is@gmail.com>
2020-03-03 02:04:37 +0900

    wip

--- test/models/MessageSpec.scala
+++ test/models/MessageSpec.scala
@@ -4,68 +4,49 @@ import org.scalatest._
 import org.scalatestplus.play._
 import play.api.test._
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.db.evolutions._
-import play.api.Configuration
+import play.api.db.DBApi
 import play.api.db.slick.DatabaseConfigProvider
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration._
-import slick.jdbc.JdbcProfile
-import com.redis._
 
-trait MysqlDatabaseBuilder extends BeforeAndAfterAll  { this: Suite =>
-  // create database
-  // drop database
-  // Database.forURL("jdbc:mysql://mysql:3306/?useSSL=false")
-  override def beforeAll(): Unit = {
-    println(11111)
-    super.beforeAll()
-  }
-
-  override def afterAll(): Unit = {
-    println(44444)
-    super.afterAll()
-  }
-}
-
-trait MysqlTableBuilder extends BeforeAndAfterEach { this: Suite =>
-  // create table with evolution
-  // drop table with evolution
-  // private val port = config.get[Int]("redis.port")
-  // https://stackoverflow.com/questions/20247742/slick-create-database
-  // https://www.playframework.com/documentation/ja/2.4.x/ScalaTestingWithDatabases
+trait DatabaseFlusher extends BeforeAndAfterEach { this: Suite =>
   val app = new GuiceApplicationBuilder().build()
   val dbConfigProvider = app.injector.instanceOf[DatabaseConfigProvider]
-  val config = app.injector.instanceOf[Configuration]
-  println(config.get[String]("slick.dbs.default.db.url"))
-  // val db = dbConfigProvider.get[JdbcProfile].db
+  val db = app.injector.instanceOf[DBApi]
   override def beforeEach(): Unit = {
-    println(22222)
+    println(11111)
     super.beforeEach()
   }
 
   override def afterEach(): Unit = {
-    println(33333)
-    // Evolutions.cleanupEvolutions(db)
+    // https://www.playframework.com/documentation/2.8.x/ScalaTestingWithDatabases
+    // https://www.programcreek.com/scala/play.api.db.DBApi
+    println(22222)
+    // println(db.database("default"))
+    db.database("default").withConnection { conn =>
+      // conn.createStatement().executeQuery("truncate table messages;")
+      conn.createStatement().executeQuery("select * from messages;")
+    }
     super.afterEach()
   }
 }
 
-class MessageSpec extends PlaySpec with MysqlDatabaseBuilder with MysqlTableBuilder {
+class MessageSpec extends PlaySpec with DatabaseFlusher {
   "Message#create" should {
     "create message" in {
-      // val model = new MessageRepository(dbConfigProvider)
-      // val created = Await.result(model.create("test message!"), Duration.Inf)
-      // created must equal (Message(1, "test message!"))
+      val model = new MessageRepository(dbConfigProvider)
+      val created = Await.result(model.create("test message!"), Duration.Inf)
+      created must equal (Message(1, "test message!"))
     }
   }
 
   // "Message#list" should {
   //   "list messages" in {
   //     val model = new MessageRepository(dbConfigProvider)
-  //     Await.result(model.create("test message!"), Duration.Inf)
-  //     val listed = Await.result(model.list(), Duration.Inf)
-  //     listed must contain (Message(1, "test message!"))
+  //     // Await.result(model.create("test message!"), Duration.Inf)
+  //     // val listed = Await.result(model.list(), Duration.Inf)
+  //     // listed must contain (Message(1, "test message!"))
   //   }
   // }
 }
