ee854840a19a8ae5a7e9cb0b9021977d9d3c57f9
KentFujii <kent.where.the.light.is@gmail.com>
2020-03-03 02:12:52 +0900

    update: integration_test

--- test/models/MessageSpec.scala
+++ test/models/MessageSpec.scala
@@ -14,19 +14,9 @@ trait DatabaseFlusher extends BeforeAndAfterEach { this: Suite =>
   val app = new GuiceApplicationBuilder().build()
   val dbConfigProvider = app.injector.instanceOf[DatabaseConfigProvider]
   val db = app.injector.instanceOf[DBApi]
-  override def beforeEach(): Unit = {
-    println(11111)
-    super.beforeEach()
-  }
-
   override def afterEach(): Unit = {
-    // https://www.playframework.com/documentation/2.8.x/ScalaTestingWithDatabases
-    // https://www.programcreek.com/scala/play.api.db.DBApi
-    println(22222)
-    // println(db.database("default"))
     db.database("default").withConnection { conn =>
-      // conn.createStatement().executeQuery("truncate table messages;")
-      conn.createStatement().executeQuery("select * from messages;")
+      conn.createStatement().executeUpdate("truncate table messages;")
     }
     super.afterEach()
   }
@@ -41,12 +31,12 @@ class MessageSpec extends PlaySpec with DatabaseFlusher {
     }
   }
 
-  // "Message#list" should {
-  //   "list messages" in {
-  //     val model = new MessageRepository(dbConfigProvider)
-  //     // Await.result(model.create("test message!"), Duration.Inf)
-  //     // val listed = Await.result(model.list(), Duration.Inf)
-  //     // listed must contain (Message(1, "test message!"))
-  //   }
-  // }
+  "Message#list" should {
+    "list messages" in {
+      val model = new MessageRepository(dbConfigProvider)
+      Await.result(model.create("test message!"), Duration.Inf)
+      val listed = Await.result(model.list(), Duration.Inf)
+      listed must contain (Message(1, "test message!"))
+    }
+  }
 }
