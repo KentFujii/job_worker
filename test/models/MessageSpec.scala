7854c83d336bb6a8596c43761b043bb586b5d198
KentFujii <kent.where.the.light.is@gmail.com>
2020-03-12 01:37:53 +0900

    wip

--- test/models/MessageSpec.scala
+++ test/models/MessageSpec.scala
@@ -19,13 +19,7 @@ trait MySqlHelper extends BeforeAndAfterEach { this: Suite =>
   // https://stackoverflow.com/questions/15338315/parallel-test-runner-for-play-framework
   // https://github.com/KentFujii/job_worker/blob/8acfb01775ac7a266a0242c11bab76551c6b39c4/test/models/MessageSpec.scala
   val dbName = s"job_worker_test_${Math.abs(Random.nextInt)}"
-  println(11111)
-  println(dbName)
-  println(22222)
-  val result = s"mysql -h mysql -uroot -ppassword -e 'create database ${dbName};'".!
-  println(33333)
-  println(result)
-  println(44444)
+  s"mysql -h mysql -uroot -ppassword -e 'create database ${dbName};'".!
   // override def afterEach(): Unit = {
   //   db.database("default").withConnection { conn =>
   //     conn.createStatement().executeUpdate("truncate table messages;")
